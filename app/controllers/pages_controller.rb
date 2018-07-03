41e2f4831804d740eb48f41650450c86301143a8
petitlapin86 <skinnerpa86@yahoo.com>
2018-07-03 14:09:19 -0400

    Add posts from all branches in the home page - Modify the _main_content.html.erb file - Define instance variables inside the PagesController’s index action - Modify the no_posts_partial_path helper method to be more reusable - Add CSS to style the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
 
-  def index
-      @posts = Post.limit(5)
-  end
 
-end
+    def index
+      @inspiration_posts = Post.by_branch('inspiration').limit(8)
+      @recipes_posts = Post.by_branch('recipes').limit(8)
+      @tiugirls_posts = Post.by_branch('tiugirls').limit(8)
+    end
+
+  end 
