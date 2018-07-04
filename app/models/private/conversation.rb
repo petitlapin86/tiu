1a9a5a7878468b7c98cc0af650d24f3eb6757f62
petitlapin86 <skinnerpa86@yahoo.com>
2018-07-04 12:13:35 -0400

    merging branches to resolve migration issue

---
+++ app/models/private/conversation.rb
@@ -0,0 +1,9 @@
+class Private::Conversation < ApplicationRecord
+  has_many :messages,
+       class_name: "Private::Message",
+       foreign_key: :conversation_id
+  belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+
+    self.table_name = 'private_conversations'
+end
---
+++ app/models/private/message.rb
@@ -0,0 +1,7 @@
+class Private::Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :conversation,
+         class_name: 'Private::Conversation',
+         foreign_key: :conversation_id
+    self.table_name = 'private_messages'
+end
