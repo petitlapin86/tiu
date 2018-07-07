7b7b0af1ebedb53bb93194f3649f468843aa1034
petitlapin86 <skinnerpa86@yahoo.com>
2018-07-07 18:23:53 -0400

    error fix controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
       @inspiration_posts = Post.by_branch('inspiration').limit(8)
       @recipes_posts = Post.by_branch('recipes').limit(8)
       @tiugirls_posts = Post.by_branch('tiugirls').limit(8)
+      @contacts = user_signed_in? ? current_user.all_active_contacts : ''
     end
 
-  end 
+  end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,66 +1,67 @@
 class PostsController < ApplicationController
-before_action :redirect_if_not_signed_in, only: [:new]
+  before_action :redirect_if_not_signed_in, only: [:new]
 
-def show
-   @post = Post.find(params[:id])
-   if user_signed_in?
-     @message_has_been_sent = conversation_exist?
-   end
- end
-
-def new
-  @branch = params[:branch]
-  @categories = Category.where(branch: @branch)
-  @post = Post.new
-end
-
-def create
-  @post = Post.new(post_params)
-  if @post.save
-    redirect_to post_path(@post)
-  else
-    redirect_to root_path
+  def show
+    @post = Post.find(params[:id])
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
   end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  def inspiration
+    posts_for_branch(params[:action])
+  end
+
+  def recipes
+    posts_for_branch(params[:action])
+  end
+
+  def tiugirls
+    posts_for_branch(params[:action])
+  end
+
+  private
+
+  def conversation_exist?
+    Private::Conversation.between_users(current_user.id, @post.user.id).present?
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'posts/posts_pagination_page' }
+    end
+  end
+
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
+
 end
-
-def inspiration
-  posts_for_branch(params[:action])
-end
-
-def recipes
-  posts_for_branch(params[:action])
-end
-
-def tiugirls #this calls a method which will return data for the specific page depending on the actions name
-  posts_for_branch(params[:action])
-end
-
-private
-
-def posts_for_branch(branch) #retrieving all categories for the branch
-  @categories = Category.where(branch: branch)
-  @posts = get_posts.paginate(page: params[:page])
-  respond_to do |format|
-  format.html
-  format.js { render partial: 'posts/posts_pagination_page' }
-end
-end
-
-def get_posts #gets posts
-  PostsForBranchService.new({
-       search: params[:search],
-       category: params[:category],
-       branch: params[:action]
-     }).call
-   end
-
-   def post_params
-  params.require(:post).permit(:content, :title, :category_id)
-                       .merge(user_id: current_user.id)
-end
-
-def conversation_exist?
-  Private::Conversation.between_users(current_user.id, @post.user.id).present?
-end
-
-end #end post controller class
