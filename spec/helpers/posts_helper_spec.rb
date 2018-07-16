7ba4bcafbc1f0602c9a6445dc4ae39d1225704f7
petitlapin86 <skinnerpa86@yahoo.com>
2018-07-16 16:06:45 -0400

    adding more test specs to group messaging

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -36,100 +36,97 @@ RSpec.describe PostsHelper, :type => :helper do
 
   context '#no_posts_partial_path' do
     it "returns a no_posts partial's path" do
-      assign(:posts, [])
-      expect(helper.no_posts_partial_path).to (
-        eq 'posts/branch/no_posts'
+      expect(helper.no_posts_partial_path([])).to (
+        eq 'posts/shared/no_posts'
       )
     end
 
     it "returns an empty partial's path" do
-      assign(:posts, [1])
-      expect(helper.no_posts_partial_path).to (
+      expect(helper.no_posts_partial_path([1])).to (
         eq 'shared/empty_partial'
       )
     end
   end
 
   context '#post_format_partial_path' do
-  it "returns a home_page partial's path" do
-    helper.stub(:current_page?).and_return(true)
-    expect(helper.post_format_partial_path).to (
-      eq 'posts/post/home_page'
-    )
+    it "returns a home_page partial's path" do
+      helper.stub(:current_page?).and_return(true)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/home_page'
+      )
+    end
+
+    it "returns a branch_page partial's path" do
+      helper.stub(:current_page?).and_return(false)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/branch_page'
+      )
+    end
   end
 
-  it "returns a branch_page partial's path" do
-    helper.stub(:current_page?).and_return(false)
-    expect(helper.post_format_partial_path).to (
-      eq 'posts/post/branch_page'
-    )
-  end
-end
+  context '#update_pagination_partial_path' do
+    it "returns an update_pagination partial's path" do
+      posts = double('posts', :next_page => 2)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/update_pagination'
+      )
+    end
 
-
-context '#update_pagination_partial_path' do
-  it "returns an update_pagination partial's path" do
-    posts = double('posts', :next_page => 2)
-    assign(:posts, posts)
-    expect(helper.update_pagination_partial_path).to(
-      eq 'posts/posts_pagination_page/update_pagination'
-    )
-  end
-
-  it "returns a remove_pagination partial's path" do
-    posts = double('posts', :next_page => nil)
-    assign(:posts, posts)
-    expect(helper.update_pagination_partial_path).to(
-      eq 'posts/posts_pagination_page/remove_pagination'
-    )
-  end
-end
-
-context '#contact_user_partial_path' do
-  before(:each) do
-    @current_user = create(:user, id: 1)
-    helper.stub(:current_user).and_return(@current_user)
+    it "returns a remove_pagination partial's path" do
+      posts = double('posts', :next_page => nil)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/remove_pagination'
+      )
+    end
   end
 
-  it "returns a contact_user partial's path" do
-    helper.stub(:user_signed_in?).and_return(true)
-    assign(:post, create(:post, user_id: create(:user, id: 2).id))
-    expect(helper.contact_user_partial_path).to(
-      eq 'posts/show/contact_user'
-    )
+  context '#contact_user_partial_path' do
+    before(:each) do
+      @current_user = create(:user, id: 1)
+      helper.stub(:current_user).and_return(@current_user)
+    end
+
+    it "returns a contact_user partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: create(:user, id: 2).id))
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/contact_user'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: @current_user.id))
+
+      expect(helper.contact_user_partial_path).to(
+        eq 'shared/empty_partial'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(false)
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/login_required'
+      )
+    end
   end
 
-  it "returns an empty partial's path" do
-    helper.stub(:user_signed_in?).and_return(true)
-    assign(:post, create(:post, user_id: @current_user.id))
+  context '#leave_message_partial_path' do
+    it "returns an already_in_touch partial's path" do
+      assign('message_has_been_sent', true)
+      expect(helper.leave_message_partial_path).to(
+        eq 'posts/show/contact_user/already_in_touch'
+      )
+    end
 
-    expect(helper.contact_user_partial_path).to(
-      eq 'shared/empty_partial'
-    )
+    it "returns an already_in_touch partial's path" do
+      assign('message_has_been_sent', false)
+      expect(helper.leave_message_partial_path).to(
+        eq 'posts/show/contact_user/message_form'
+      )
+    end
   end
-
-  it "returns an empty partial's path" do
-    helper.stub(:user_signed_in?).and_return(false)
-    expect(helper.contact_user_partial_path).to(
-      eq 'posts/show/login_required'
-    )
-  end
-end
-
-context '#leave_message_partial_path' do
-  it "returns an already_in_touch partial's path" do
-    assign('message_has_been_sent', true)
-    expect(helper.leave_message_partial_path).to(
-      eq 'posts/show/contact_user/already_in_touch'
-    )
-  end
-
-  it "returns an already_in_touch partial's path" do
-    assign('message_has_been_sent', false)
-    expect(helper.leave_message_partial_path).to(
-      eq 'posts/show/contact_user/message_form'
-    )
-  end
-end
 
 end
--- spec/jobs/private/message_broadcast_job_spec.rb
+++ spec/jobs/private/message_broadcast_job_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Private::MessageBroadcastJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Private::MessageBroadcastJob, :type => :job do
+  context '#perform_later' do
+    let(:message) { create(:private_message) }
+
+    it 'enqueues a job' do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        message
+      }.to have_enqueued_job
+    end
+  end
 end
